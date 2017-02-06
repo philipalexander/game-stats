097228059af4994f11e2ad93ca02e4c1a04ac059
Philip Alexander <philip.j.alexander@accenture.com>
2017-02-06 16:46:20 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule }  from '@angular/platform-browser';
+import { NgModule }       from '@angular/core';
+import { FormsModule }    from '@angular/forms';
+import { HttpModule }     from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { StatsService }         from './stats.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [StatsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/mock.ts
@@ -0,0 +1,8 @@
+import { Player } from './player';
+
+export const PLAYERS: Player[] = [
+  { id: 11, name: 'Philip', points: 12 },
+  { id: 12, name: 'Wes', points: 13 },
+  { id: 13, name: 'Tom', points: 121 },
+  { id: 14, name: 'Zach', points: 0},
+];
